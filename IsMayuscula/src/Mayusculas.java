68b891baf7271e89b7e3beee80121d05db45dcd0
Unknown <pedrodlafuente@gmail.com>
2018-03-12 20:13:54 +0100

    Mayusculas

--- IsMayuscula/src/Mayusculas.java
+++ IsMayuscula/src/Mayusculas.java
@@ -2,7 +2,17 @@
 public class Mayusculas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		char a = 'a';
+		char b = 'A';
+		char[] caracteres = {a,b};
+		
+		for (char c : caracteres) {
+			if(Character.isUpperCase(c))System.out.println("La letra "+c+" es masyuscula");
+			else System.out.println("La letra "+c+" es minuscula");
+		}
+		
+		
+		
 
 	}
 
